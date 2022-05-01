5fd4540970270752d8a3929e77b81181b7ccb320
SaraR <sg.ramirez940@uniandes.edu.co>
2022-05-01 12:53:23 -0500

    pruebas componente vehículos (punto 3)

--- src/app/vehiculo/vehiculo-list/vehiculo-list.component.spec.ts
+++ src/app/vehiculo/vehiculo-list/vehiculo-list.component.spec.ts
@@ -2,16 +2,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 import { VehiculoListComponent } from './vehiculo-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { Vehiculo } from '../vehiculo';
+import { VehiculoService } from '../vehiculo.service';
 
 describe('VehiculoListComponent', () => {
   let component: VehiculoListComponent;
   let fixture: ComponentFixture<VehiculoListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ VehiculoListComponent ]
+      imports: [HttpClientModule],
+      declarations: [ VehiculoListComponent ],
+      providers: [ VehiculoService ]
     })
     .compileComponents();
   }));
@@ -19,10 +26,56 @@ describe('VehiculoListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(VehiculoListComponent);
     component = fixture.componentInstance;
+
+    component.vehiculos = [
+      new Vehiculo(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.image.imageUrl()
+
+      ),
+      new Vehiculo(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.image.imageUrl()
+
+      ),
+      new Vehiculo(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.image.imageUrl()
+
+      ),
+    ];
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+   it('should have 3 rows', () => {
+
+    const rowDebugElements = debug.queryAll(By.css('tbody tr'));
+    expect(rowDebugElements.length).toBe(3);
+
+
   });
 });
