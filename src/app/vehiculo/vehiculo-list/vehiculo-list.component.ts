0e8e273986dc327f7e377a23d184c0f07fc8db6b
SaraR <sg.ramirez940@uniandes.edu.co>
2022-04-30 20:36:48 -0500

    creación servicio vehículo y conexión con backend

--- src/app/vehiculo/vehiculo-list/vehiculo-list.component.ts
+++ src/app/vehiculo/vehiculo-list/vehiculo-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Vehiculo } from '../vehiculo';
+import { VehiculoService } from '../vehiculo.service';
 
 @Component({
   selector: 'app-vehiculo-list',
@@ -9,9 +10,16 @@ import { Vehiculo } from '../vehiculo';
 export class VehiculoListComponent implements OnInit {
 
   vehiculos: Array<Vehiculo> = [];
-  constructor() { }
+  constructor(private vehiculService: VehiculoService) { }
+
+  getVehiculos(): void {
+    this.vehiculService.getVehiculos().subscribe((vehiculos) => {
+      this.vehiculos = vehiculos;
+    });
+  }
 
   ngOnInit() {
+    this.getVehiculos();
   }
 
 }
